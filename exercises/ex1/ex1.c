e2e8a131efc364f897af9b576fdc8df2ed29866e
arj1 <arj1@localhost.localdomain>
2022-01-05 17:23:02 -0800

    added exercies 1

---
+++ exercises/ex1/ex1.c
@@ -0,0 +1,54 @@
+/*
+Arjun Srivastava
+arj1@uw.edu
+CSE 333
+Copyright 2022 Arjun Srivastava
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Estimates the value of pi using Nilakantha infinite series
+double estimate(int n);
+
+// Returns nth term of pi infinite series
+double nthTerm(int n);
+
+int main(int argc, char* argv[]) {
+    int n;
+    // Only run if there are arguments
+    for (char** arg = argv + 1; *arg; ++arg) {
+        int num;
+        // Extract integer from argument (ignore trailing alphanumeric chars)
+        sscanf(*arg, "%d", &num);
+        // If we reach a positive integer value, return estimate
+        if (num > 0) {
+            n = num;
+            double result = estimate(n);
+            printf("Our estimate of Pi is %f\n", result);
+            return EXIT_SUCCESS;
+        }
+    }
+    printf("Please provide a valid input (positive integer) \n");
+    return EXIT_FAILURE;    // If there are no valid inputs
+}
+
+double estimate(int n) {
+    double result = 3;
+    int i;
+    for (i = 1; i <= n; i++) {
+        result += nthTerm(i);
+    }
+    return result;
+}
+
+double nthTerm(int n) {
+    double multiplier;
+    if (n % 2 == 1) {
+        multiplier = 1;
+    } else {
+        multiplier = -1;
+    }
+    return multiplier * (4.0 / (2.0*n * (2.0*n + 1.0) * (2.0*n + 2.0)));
+}
