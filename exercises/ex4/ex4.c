7c667ee3a0c0dacaabc08040274d7be23dfb4518
arj1 <arj1@ue.edu>
2022-01-20 02:42:17 -0800

    started ex4

---
+++ exercises/ex4/ex4.c
@@ -0,0 +1,79 @@
+#include <stdio.h>    // for snprintf
+#include <stdlib.h>   // for EXIT_SUCCESS, NULL
+#include <string.h>   // for strrchr, strcmp, strlen
+#include <stdbool.h>  // for bool
+
+#include "ro_file.h"
+
+
+/*** HELPER FUNCTION DECLARATIONS ******************************************/
+
+// Returns whether or not a filename ends in ".txt".
+bool IsTxtFile(char* filename);
+
+// Concatenate the directory and file names into a full path. The caller is
+// responsible for freeing the allocated string. Exits if an error occurs.
+char* Concatenate(char* dirname, char* filename);
+
+// Usage/Error functions
+void Usage();
+
+void Args();
+
+/*
+ * This program:
+ * - Accepts a directory name as a command-line argument.
+ * - Scans through the directory looking for all files whose names end with
+ *   the four characters ".txt".
+ * - For every such file found, write the contents of those files to stdout
+ *   without adding any additional characters or formatting.
+ *   Eventually reading the files with ro_file module.
+ */
+int main(int argc, char** argv) {
+  char* dirname;
+  if (argc != 2) {
+    Usage();
+    Args();
+  }
+
+  return EXIT_SUCCESS;
+}
+
+
+/*** HELPER FUNCTION DEFINITIONS *******************************************/
+
+bool IsTxtFile(char* filename) {
+  char* dot = strrchr(filename, '.');
+  return dot && !strcmp(dot, ".txt");
+}
+
+char* Concatenate(char* dirname, char* filename) {
+  bool has_trailing_slash = dirname[strlen(dirname) - 1] == '/';
+  // Concatenate directory and file name.
+  size_t dlen = strlen(dirname);
+  size_t flen = strlen(filename);
+  // Malloc space for full path name:
+  // dlen + strlen("/") + flen + strlen('\0') = dlen + flen + 2
+  int size_to_malloc = has_trailing_slash ? dlen + flen + 1 : dlen + flen + 2;
+  char* fullpath = (char*) malloc(sizeof(char) * (size_to_malloc));
+  if (fullpath == NULL) {
+    fprintf(stderr, "Error on malloc.\n");
+    exit(EXIT_FAILURE);
+  }
+  if (has_trailing_slash) {
+    snprintf(fullpath, size_to_malloc, "%s%s", dirname, filename);
+  } else {
+    snprintf(fullpath, size_to_malloc, "%s/%s", dirname, filename);
+  }
+  return fullpath;
+}
+
+void Usage() {
+  fprintf(stderr, "Usage: ./ex4 [dir_name]\n");
+}
+
+void Args() {
+  printf("This program accepts 1 argument corresponding to a directory name\n");
+}
+
+// Add error message if no txt files found?
