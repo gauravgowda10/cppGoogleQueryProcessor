fe2d6b84b439257c585f4c5690a94236d7439c43
arj1 <arj1@ue.edu>
2022-02-11 14:52:41 -0800

    finished ex8

--- exercises/ex8/ex8.cc
+++ exercises/ex8/ex8.cc
@@ -5,11 +5,12 @@ CSE 333
 Copyright 2022 Arjun Srivastava
 */
 
-#include <iostream> 
+#include <iostream>
 #include <cstdlib>  // for EXIT_SUCCESS
 #include <map>
 #include <fstream>
 #include <string>
+#include <algorithm>
 
 using std::cout;
 using std::cin;
@@ -18,16 +19,16 @@ using std::endl;
 using std::ifstream;
 using std::string;
 using std::map;
+using std::pair;
+using std::for_each;
 
-// ADD HELPERs
 
 // Reads the current value from the stream into output
 template <typename T>
 bool ReadValue(ifstream& in, T* output);
 
-// Accepts a sorted map and prints the outputs
-template <typename T, int N>
-void PrintWordCounts(map<T, N>& wordOccurrences);
+// Accepts a map and prints the key/value pairs
+void PrintOut(const pair<string, int>& p);
 
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -38,32 +39,43 @@ int main(int argc, char** argv) {
     ifstream in;
     in.open(filename);
 
-    if (!in.is_open()) {
+    if (!in.is_open() || !in.good()) {
         cerr << "Error opening file" << endl;
         exit(EXIT_FAILURE);
     }
 
-    // Check other errors when opening file (good, bad, eof)
-
     string data;
-    while (in >> data) {
+    map<string, int> word_occurences;
+
+    while (!in.eof()) {
         bool read = ReadValue(in, &data);
         if (!read) {
             cerr << "Error reading from file" << endl;
             exit(EXIT_FAILURE);
         }
-        
+        if (word_occurences.count(data)) {
+            word_occurences[data]++;
+        } else {
+            word_occurences[data] = 1;
+        }
     }
     in.close();
-    
+    for_each(word_occurences.begin(), word_occurences.end(), &PrintOut);
+
     return EXIT_SUCCESS;
 }
 
 template <typename T>
 bool ReadValue(ifstream& in, T* output) {
+    T data;
+    in >> data;
+    if (in.bad()) {
+        return false;
+    }
+    *output = data;
     return true;
 }
 
-void PrintWordCounts(map<T, N>& wordOccurrences) {
-
+void PrintOut(const pair<string, int>& p) {
+    cout << p.first << " " << p.second << endl;
 }
